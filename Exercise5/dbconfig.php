4127afe1bfa8831e0d54e117c4b11da51aea0e7a
ar <deleonaleoralphcastro@gmail.com>
2016-11-29 10:51:03 +0800

    update for exercise 5

--- Exercise5/dbconfig.php
+++ Exercise5/dbconfig.php
@@ -1,28 +1,9 @@
-<?php
-include_once 'dbconfig.php';
-if(isset($_POST['btn-save']))
-{
- // variables for input data
- $first_name = $_POST['first_name'];
- $last_name = $_POST['last_name'];
- $city_name = $_POST['city_name'];
- // variables for input data
- 
- // sql query for inserting data into database
- 
-        $sql_query = "INSERT INTO users(first_name,last_name,user_city) VALUES('$first_name','$last_name','$city_name')";
- mysql_query($sql_query);
-        
-        // sql query for inserting data into database
- 
-}
-?>
 
 <?php
 $host = "localhost";
 $user = "root";
 $password = "";
 $datbase = "dbtuts";
-mysql_connect($host,$user,$password);
-mysql_select_db($datbase);
+$con=mysqli_connect($host,$user,$password);
+mysqli_select_db($con,$datbase);
 ?>
