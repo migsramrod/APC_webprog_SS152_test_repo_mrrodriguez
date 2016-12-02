3fe1e1ce140005e64642731d4d437823dd1f7c4e
migramrod <migsramrod>
2016-12-02 11:15:36 +0800

    editing index.php

--- Exercise5/index.php
+++ Exercise5/index.php
@@ -15,7 +15,7 @@ if(isset($_GET['delete_id']))
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>CRUD Operations With PHP and MySql - By Cleartuts</title>
+<title>CRUD Operations With PHP and MySql</title>
 <link rel="stylesheet" href="style.css" type="text/css" />
 <script type="text/javascript">
 function edt_id(id)
