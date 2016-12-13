98a34ebda83f6b0873ebe5b56caa803ab4d743a1
migsramrod <migsramrod>
2016-12-13 11:02:09 +0800

    Fixing the fields of index.php

--- Exercise5/index.php
+++ Exercise5/index.php
@@ -49,9 +49,13 @@ function delete_id(id)
     <tr>
     <th colspan="5"><a href="add_data.php">add data here.</a></th>
     </tr>
-    <th>First Name</th>
-    <th>Last Name</th>
-    <th>City Name</th>
+    <th>Complete Name</th>
+    <th>Nickname</th>
+    <th>Email Address</th>
+	<th>Home Address</th>
+	<th>Gender</th>
+	<th>Cellphone</th>
+	  <th>Comments</th>
     <th colspan="2">Operations</th>
     </tr>
     <?php
@@ -64,6 +68,10 @@ function delete_id(id)
         <td><?php echo $row[1]; ?></td>
         <td><?php echo $row[2]; ?></td>
         <td><?php echo $row[3]; ?></td>
+		<td><?php echo $row[4]; ?></td>
+		<td><?php echo $row[5]; ?></td>
+	    <td><?php echo $row[6]; ?></td>
+	    <td><?php echo $row[7]; ?></td>
   <td align="center"><a href="javascript:edt_id('<?php echo $row[0]; ?>')"><img src="b_edit.png" align="EDIT" /></a></td>
         <td align="center"><a href="javascript:delete_id('<?php echo $row[0]; ?>')"><img src="b_drop.png" align="DELETE" /></a></td>
         </tr>
