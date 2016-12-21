1500ab73032d152eaa27de30bdff797de6aacab7
migsramrod <migsramrod>
2016-12-22 06:48:45 +0800

    debugging the index.php

--- Exercise6/CodeIgniter-3.1.2/application/views/news/index.php
+++ Exercise6/CodeIgniter-3.1.2/application/views/news/index.php
@@ -1,21 +1,24 @@
-h2><?php echo $title; ?></h2>
+<h2><?php echo $title; ?></h2>
+ 
 <table border='1' cellpadding='4'>
-    <tr>
-        <td><strong>Title</strong></td>
-        <td><strong>Content</strong></td>
-        <td><strong>Action</strong></td>
-    </tr>
+    <tr>
+        <td><strong>Title</strong></td>
+        <td><strong>Content</strong></td>
+        <td><strong>Action</strong></td>
+    </tr>
 <?php foreach ($news as $news_item): ?>
-        <tr>
-            <td><?php echo $news_item['title']; ?></td>
-            <td><?php echo $news_item['text']; ?></td>
-            <td>
-                <a href="<?php echo site_url('news/'.$news_item['slug']); ?>">
-View</a> |
-                <a href="<?php echo site_url('news/edit/'.$news_item['id']);
-?>">Edit</a> |
-                <a href="<?php echo site_url('news/delete/'.$news_item['id']);
-?>" onClick="return confirm('Are you sure you want to delete?')">Delete</a>
-            </td>
-        </tr>
-<?php endforeach;
+        <tr>
+            <td><?php echo $news_item['title']; ?></td>
+            <td><?php echo $news_item['text']; ?></td>
+            <td>
+                <a href="<?php echo site_url('news/'.$news_item['slug']); ?>">View</a> | 
+                <a href="<?php echo site_url('news/edit/'.$news_item['id']); ?>">Edit</a> | 
+                <a href="<?php echo site_url('news/delete/'.$news_item['id']); ?>" onClick="return confirm('Are you sure you want to delete?')">Delete</a>
+            </td>
+        </tr>
+<?php endforeach; ?>
+</table>
+
+<?php
+echo '<h2>'.$news_item['title'].'</h2>';
+echo $news_item['text'];
